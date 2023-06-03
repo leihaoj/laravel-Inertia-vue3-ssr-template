f26de20d5bef7676ce6189a4911c713b819b2b09
haojie <haojie@qq.com>
2023-06-03 09:48:38 +0800

    1

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,20 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// 清除 opcache 全部缓存
+Artisan::command('clear-all:opcache', function () {
+    opcache_reset();
+    $this->comment('OPcache 所有缓存已清除');
+})->purpose('Clear all OPcache');
+
+// 清除掉最近一次更新文件的缓存
+Artisan::command('clear:opcache', function () {
+    exec('git diff --name-only HEAD~ HEAD', $output);
+    foreach ($output as $file) {
+        $path = base_path($file);
+        opcache_invalidate($path, true);
+    }
+    dd('刷新完成');
+})->purpose('Clear OPcache');
