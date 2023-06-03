077d52cee738a50c5bca5647e8ccce5d76309b52
haojie <haojie@qq.com>
2023-06-03 10:28:34 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import ViteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
     plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
             // ssr: 'resources/js/ssr.js',
             refresh: true, // 保存时刷新
         }),
+        ViteCompression(),
     ],
     server: {
         port: 3008,
