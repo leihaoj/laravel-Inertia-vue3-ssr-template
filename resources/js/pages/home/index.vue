e874b4e0064b507e3e3d348443d85ab7420a0795
haojie[ <haojie>
2023-08-19 17:55:00 +0800

    加入.prettierrc和tsx配置

--- resources/js/pages/home/index.vue
+++ resources/js/pages/home/index.vue
@@ -1,24 +1,23 @@
 <template>
-    <Head :title="info.title"/>
-    <h1>{{ info.title }}</h1>
-    {{ num }}
-    <button @click="onNumChange">点击</button>
-    <p>新的内容是否加载</p>
+  <Head :title="info.title" />
+  <h1>{{ info.title }}</h1>
+  {{ num }}
+  <button @click="onNumChange">点击</button>
+  <p>新的内容是否加载</p>
 </template>
 
-
 <script setup lang="ts">
-import {Head} from '@inertiajs/vue3'
-import {ref} from 'vue';
+import { Head } from "@inertiajs/vue3";
+import { ref } from "vue";
 
-defineProps({info: Object})
+const props = defineProps({ info: Object });
 
-const dialog_visible = ref<boolean>(false)
-const num = ref(1)
+console.log(props.info);
+
+const dialog_visible = ref<boolean>(false);
+const num = ref(1);
 
 const onNumChange = () => {
-    num.value += 1;
-}
+  num.value += 1;
+};
 </script>
-
-
